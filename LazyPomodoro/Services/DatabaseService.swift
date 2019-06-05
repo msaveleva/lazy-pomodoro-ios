9192fa7221dfd76d27fd503da23574cff5dd3e21
Maria Saveleva <mariiasaveleva@gmail.com>
2019-06-05 18:26:50 +0300

    added all three sorting methods for pomodoros

--- LazyPomodoro/Services/DatabaseService.swift
+++ LazyPomodoro/Services/DatabaseService.swift
@@ -53,12 +53,39 @@ class DatabaseService {
     }
     
     func loadTodaysPomodoros() -> Single<[Pomodoro]> {
+        let calendar = Calendar.current
+        let fromDate = calendar.startOfDay(for: Date()) as NSDate
+        
+        let predicate = NSPredicate(format: "date >= %@", fromDate)
+        return loadPomodoros(with: predicate)
+    }
+    
+    func loadThisWeekPomodoros() -> Single<[Pomodoro]> {
+        let calendar = Calendar.current
+        let fromDate = Date()
+        guard let toDate = calendar.date(byAdding: .day, value: -7, to: fromDate) else {
+            fatalError("Can't receive the correct date.")
+        }
+        
+        let predicate = NSPredicate(format: "date <= %@ && date >= %@", fromDate as NSDate, toDate as NSDate)
+        return loadPomodoros(with: predicate)
+    }
+    
+    func loadThisMonthPomodoros() -> Single<[Pomodoro]> {
+        let fromDate = Date()
+        guard let startOfTheMonth = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: fromDate))) else {
+            fatalError("Can't receive the correct date.")
+        }
+        
+        let predicate = NSPredicate(format: "date <= %@ && date >= %@", fromDate as NSDate, startOfTheMonth as NSDate)
+        return loadPomodoros(with: predicate)
+    }
+    
+    //MARK: - Private methods
+    
+    private func loadPomodoros(with predicate: NSPredicate) -> Single<[Pomodoro]> {
         return Single.create { [weak self] single in
-            let calendar = Calendar.current
-            let fromDate = calendar.startOfDay(for: Date()) as NSDate
-            
-            let todayPredicate = NSPredicate(format: "date >= %@", fromDate)
-            if let pomodoros = self?.realm.objects(Pomodoro.self).filter(todayPredicate).toArray() {
+            if let pomodoros = self?.realm.objects(Pomodoro.self).filter(predicate).toArray() {
                 single(.success(pomodoros))
             } else {
                 single(.success([Pomodoro]()))
